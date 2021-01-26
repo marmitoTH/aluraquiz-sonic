21825192475ba40f651694e6bbf59546371f02f4
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-26 19:23:50 -0300

    Added Head component in app entry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app'
+import Head from '../components/Head'
 import GlobalStyle from '../styles/global'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
+      <Head />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
