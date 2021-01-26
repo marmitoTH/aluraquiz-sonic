df0914bf024d91c53c61b52fe23f80d966b0e933
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-26 16:59:06 -0300

    Created base html structure to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
+import logo from '../assets/logo.svg'
+
 const Home = () => {
   return (
-    <h1>Alura Quiz</h1>
+    <div>
+      <div />
+      <main>
+        <img src={logo} alt='Logo AluraQuiz' />
+        <div>
+          <h1>Sonic The Hedgehog</h1>
+          <p>Teste os seus conhecimentos sobre o universo Sonic The Hedgehog e compare seu resultado com os amigos!</p>
+          <form>
+            <input placeholder='Diz aí seu nome pra jogar! :)' />
+            <button>JOGAR</button>
+          </form>
+        </div>
+        <div>
+          <h1>Quizes da galera</h1>
+          <p>Dá uma olhada nesses quizes incríveis que o pessoal da Imersão Alguma coisa fez:</p>
+        </div>
+      </main>
+    </div>
   )
 }
 
