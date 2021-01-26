9483e0f8f20e366742165ef763e51fc8ce6b2546
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-26 19:22:42 -0300

    Create Head component to handle title and metadata.

---
+++ src/components/Head/index.tsx
@@ -0,0 +1,13 @@
+import NextHead from 'next/head'
+
+const Head = () => {
+  return (
+    <NextHead>
+      <title>AluraQuiz - Sonic</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
+    </NextHead>
+  )
+}
+
+export default Head
