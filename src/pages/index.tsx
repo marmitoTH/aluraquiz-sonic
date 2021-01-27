c83fa8b5278a698836c23ce18ff18f2922f9ef07
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-26 21:10:25 -0300

    Created styling for home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,31 @@
+import * as Styled from '../styles/pages/home'
 import logo from '../assets/logo.svg'
 
 const Home = () => {
   return (
     <div>
-      <div />
-      <main>
-        <img src={logo} alt='Logo AluraQuiz' />
-        <div>
-          <h1>Sonic The Hedgehog</h1>
-          <p>Teste os seus conhecimentos sobre o universo Sonic The Hedgehog e compare seu resultado com os amigos!</p>
-          <form>
-            <input placeholder='Diz aí seu nome pra jogar! :)' />
-            <button>JOGAR</button>
-          </form>
-        </div>
-        <div>
-          <h1>Quizes da galera</h1>
-          <p>Dá uma olhada nesses quizes incríveis que o pessoal da Imersão Alguma coisa fez:</p>
-        </div>
-      </main>
+      <Styled.Background />
+      <Styled.Main>
+        <Styled.Logo src={logo} alt='Logo AluraQuiz' />
+        <Styled.Widget>
+          <Styled.WidgetHeader>
+            <Styled.Title>Sonic The Hedgehog</Styled.Title>
+          </Styled.WidgetHeader>
+          <Styled.WidgetContent>
+            <p>Teste os seus conhecimentos sobre o universo Sonic e compare seu resultado com os amigos!</p>
+            <Styled.Form>
+              <input placeholder='Diz aí seu nome pra jogar! :)' />
+              <button>JOGAR</button>
+            </Styled.Form>
+          </Styled.WidgetContent>
+        </Styled.Widget>
+        <Styled.Widget>
+          <Styled.WidgetContent>
+            <Styled.Title>Quizes da galera</Styled.Title>
+            <p>Dá uma olhada nesses quizes incríveis que o pessoal da Imersão Alguma coisa fez:</p>
+          </Styled.WidgetContent>
+        </Styled.Widget>
+      </Styled.Main>
     </div>
   )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,90 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+  z-index: -1;
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  background: linear-gradient(0deg, #1c1814 1%, transparent),
+              url('https://wallpapercave.com/wp/wp3543695.png');
+  background-size: cover;
+
+  h1 {
+    font-size: 16px;
+  }
+
+  @media (min-width: 1000px) {
+    height: 100vh;
+    background: url('https://wallpapercave.com/wp/wp3543695.png');
+    background-size: cover;
+  }
+`
+
+export const Widget = styled.div`
+  border-radius: 4px;
+  border: 2px solid #2869f0;
+  background-color: #1c1814;
+
+  & + & {
+    margin-top: 24px;
+  }
+`
+
+export const WidgetHeader = styled.div`
+  padding: 14px 32px;
+  background-color: #2869f0;
+`
+
+export const WidgetContent = styled.div`
+  padding: 18px 32px;
+
+  p {
+    font-size: 14px;
+    line-height: 1.4;
+  }
+`
+
+export const Main = styled.main`
+  margin: auto;
+  padding-top: 40px;
+  width: min(350px, 95%);
+
+  @media (min-width: 1000px) {
+    padding-top: 55px;
+    margin-left: 128px;
+  }
+`
+
+export const Logo = styled.img`
+  margin-bottom: 32px;
+`
+
+export const Form = styled.form`
+  margin: 32px 0;
+
+  input {
+    width: 100%;
+    height: 36px;
+    border-radius: 4px;
+    padding: 8px 16px;
+    background: none;
+    color: rgba(255, 255, 255, 0.7);
+    border: 2px solid rgba(255, 255, 255, 0.5);
+  }
+
+  button {
+    width: 100%;
+    height: 36px;
+    margin-top: 24px;
+    border-radius: 4px;
+    background: #e63c2c;
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 16px;
+  line-height: 1.8;
+`
