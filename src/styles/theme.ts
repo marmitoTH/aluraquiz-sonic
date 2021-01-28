e4ebacf49b56ff365a094a22c9761756b7892bac
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-28 20:08:09 -0300

    Created theme object to use globally in the application style

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  Images: {
+    background: 'https://wallpapercave.com/wp/wp3543695.png'
+  },
+  Colors: {
+    primaryColor: '#2869f0',
+    secondaryColor: '#e63c2c',
+    backgroundColor: '#1c1814',
+    contrastTextColor: '#fff',
+    success: '#4caf50',
+    failed: '#f85622'
+  }
+}
