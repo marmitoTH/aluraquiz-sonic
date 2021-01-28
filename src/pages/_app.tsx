ae62ad427f64ee697a0cec1ee2c2db6104799403
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-28 20:11:46 -0300

    Added theme object to the styled component theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from 'next/app'
+import theme from '../styles/theme'
+import { ThemeProvider } from 'styled-components'
 import Head from '../components/Head'
 import GlobalStyle from '../styles/global'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head />
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
