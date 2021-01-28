4d79849877c0d63c403482f88ce9d14c149133b7
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-28 17:50:10 -0300

    Removed background styling from home style sheet

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,25 +1,5 @@
 import styled from 'styled-components'
 
-export const Background = styled.div`
-  z-index: -1;
-  position: absolute;
-  width: 100%;
-  height: 200px;
-  background: linear-gradient(0deg, #1c1814 1%, transparent),
-              url('https://wallpapercave.com/wp/wp3543695.png');
-  background-size: cover;
-
-  h1 {
-    font-size: 16px;
-  }
-
-  @media (min-width: 1000px) {
-    height: 100vh;
-    background: url('https://wallpapercave.com/wp/wp3543695.png');
-    background-size: cover;
-  }
-`
-
 export const Container = styled.div`
   margin: auto;
   padding-top: 40px;
