e3bd2d8430431a9cf317838135ff38d5401c1895
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-29 18:41:11 -0300

    Created Quiz Context to control the game state globally

---
+++ src/contexts/QuizContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, Dispatch, SetStateAction, useState } from 'react'
+
+interface GameProps {
+  user: string | undefined
+  points: number | undefined
+
+  setUser: Dispatch<SetStateAction<string>>
+  setPoints: Dispatch<SetStateAction<number>>
+}
+
+export const Context = createContext<GameProps>({} as GameProps)
+
+export const Provider: React.FC = ({ children }) => {
+  const [user, setUser] = useState('')
+  const [points, setPoints] = useState(0)
+
+  return (
+    <Context.Provider value={{ user, points, setUser, setPoints }}>
+      {children}
+    </Context.Provider>
+  )
+}
