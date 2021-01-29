f305350896be80886f889689c96453ec0392b4b9
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-28 22:17:10 -0300

    Removed hyphen from app title

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -3,7 +3,7 @@ import NextHead from 'next/head'
 const Head = () => {
   return (
     <NextHead>
-      <title>AluraQuiz - Sonic</title>
+      <title>AluraQuiz Sonic</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
     </NextHead>
