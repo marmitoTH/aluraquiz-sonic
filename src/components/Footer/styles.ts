abd80429cc4b4108679fb19a3d3d6998359d32a9
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-29 17:38:45 -0300

    Updated footer styling

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,11 +4,13 @@ export const Footer = styled.footer`
   display: flex;
   padding: 20px;
   margin-top: 20px;
+  font-size: 14px;
+  line-height: 1.4;
   border-radius: ${({ theme }) => theme.borderRadius};
   background-color: rgba(0, 0, 0, .5);
 
   img {
-    margin-right: 20px;
+    margin-right: 25px;
   }
 
   a {
