dd15744b24ec6aac005b0252af049ecaea951aeb
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-30 14:03:53 -0300

    Added more metatags to the page head for better SEO

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -1,11 +1,16 @@
+import db from '../../../db.json'
 import NextHead from 'next/head'
 
 const Head = () => {
   return (
     <NextHead>
-      <title>AluraQuiz Sonic</title>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
+      <title>AluraQuiz {db.title}</title>
+      <meta name='description' content={db.description} />
+      <meta property='og:title' content={`AluraQuiz ${db.title}`} />
+      <meta property='og:description' content={db.description} />
+      <meta property='og:image' content={db.bg} />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap' rel='stylesheet' />
     </NextHead>
   )
 }
