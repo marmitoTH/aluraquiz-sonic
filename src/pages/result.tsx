b8b7e0622b343a07c7db753a032545b2f5a153b4
Lucas <lucasrodrigues123@hotmail.com.br>
2021-01-30 11:25:39 -0300

    Updated result page html structure

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,13 +1,32 @@
 import { useContext } from 'react'
 import { Context } from '../contexts/QuizContext'
+import * as Styled from '../styles/pages/result'
+import Background from '../components/Background'
+import Header from '../components/Header'
+import Container from '../components/Container'
+import GitHubCorner from '../components/GitHubCorner'
+import * as Widget from '../components/Widget'
 
 const Result = () => {
   const { user, points } = useContext(Context)
 
   return (
     <>
-      <h1>{user}</h1>
-      <p>{points}</p>
+      <Background />
+      <GitHubCorner projectUrl='https://github.com/marmitoTH/aluraquiz-sonic' />
+      <Container>
+        <Header />
+        <Widget.default>
+          <Widget.Header>
+            <strong>Resultado</strong>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Obrigado por responder o quiz, {user}!</p>
+            <Styled.Score>Você fez {points} pontos!</Styled.Score>
+            <Styled.HomeAnchor href='/'>Voltar para a home</Styled.HomeAnchor>
+          </Widget.Content>
+        </Widget.default>
+      </Container>
     </>
   )
 }
