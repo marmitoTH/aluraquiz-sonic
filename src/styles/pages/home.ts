86ec6db293f63f6e0fc9d391251ac04c0d103dc7
Lucas <lucasrodrigues123@hotmail.com.br>
2021-02-01 11:11:43 -0300

    Created style to external quiz links

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,3 +1,4 @@
+import { shade } from 'polished'
 import styled from 'styled-components'
 
 export const Logo = styled.img`
@@ -26,4 +27,26 @@ export const Title = styled.h1`
   font-size: 16px;
   line-height: 1.8;
   margin-bottom: 8px;
+`
+
+export const Description = styled.p`
+  margin-bottom: 10px;
+`
+
+export const ExternalLink = styled.a`
+  display: block;
+  padding: 9px 15px;
+  color: ${({ theme }) => theme.colors.contrastTextColor};
+  background: ${({ theme }) => shade(.5, theme.colors.primaryColor)};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  text-decoration: none;
+  transition: 150ms;
+
+  & + & {
+    margin-top: 8px;
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primaryColor};
+  }
 `
